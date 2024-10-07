495abc32d287ffba5705e7be4cbb9cc368437f5e
CheatInNeed <peterroland02@gmail.com>
2024-10-07 15:06:24 +0200

    removed for-loop

--- main.java
+++ main.java
@@ -1,8 +1,5 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
-        }
+        
     }
 }
