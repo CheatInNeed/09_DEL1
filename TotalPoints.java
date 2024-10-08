5e3884155fa057a7c320d7e94b9374ca1645e9da
CheatInNeed <peterroland02@gmail.com>
2024-10-08 16:52:32 +0200

    refined totalpoints

--- TotalPoints.java
+++ TotalPoints.java
@@ -1,34 +1,11 @@
-
 public class TotalPoints {
-
-
-<<<<<<< HEAD
-    int totalPoints = 0;
-
+    private int totalPoints = 0;
 
     public int getTotalPoints() {
-
-        return totalPoints;
-        
+        return this.totalPoints; 
     }
-
 
     public void dropPoints() {
-
         this.totalPoints = 0;
-
     }
-
-
-
-=======
-
-
-
-
-
-
-
-    
->>>>>>> a317292c61cc07931667c0602a1890d27df7ac65
 }
