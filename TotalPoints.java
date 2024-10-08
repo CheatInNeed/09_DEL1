679b110fa46f6bc3ac1e64971186086d416e5521
Flace333 <magnusfehrmann@gmail.com>
2024-10-08 16:34:25 +0200

    added TotalPoints class

---
+++ TotalPoints.java
@@ -0,0 +1,23 @@
+
+public class TotalPoints {
+
+
+    int totalPoints = 0;
+
+
+    public int getTotalPoints() {
+
+        return totalPoints;
+        
+    }
+
+
+    public void dropPoints() {
+
+        this.totalPoints = 0;
+
+    }
+
+
+
+}
