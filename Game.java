904c20e70abfdd707a1385577910f8edce0aa5f8
CheatInNeed <peterroland02@gmail.com>
2024-10-10 13:25:18 +0200

    added Game class, to init game

---
+++ Game.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Game extends Player {
+    public static void startGame(List<Player> players){
+        
+            outerLoop:
+            while (true) {
+                
+            }
+        
+    }
+}
